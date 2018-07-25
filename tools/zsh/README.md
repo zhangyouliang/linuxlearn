b017db8ef05366dd7f015143842d694645505f39
YouliangZhang <1577121881@qq.com>
2018-07-25 23:30:19 +0800

    测试

---
+++ tools/zsh/README.md
@@ -0,0 +1,3 @@
+
+
+    sh -c "$(curl -fsSL https://gitee.com/whatdy/linuxlearn/raw/master/tools/zsh/zsh.sh)"
--- tools/zsh/zsh.sh
+++ tools/zsh/zsh.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://gitee.com/whatdy/linuxlearn/raw/master/tools/zsh/install.sh)"
 cd ~/.oh-my-zsh/plugins/
-git clone git://github.com/zsh-users/zsh-autosuggestions
-
+git clone https://gitee.com/whatdy/zsh-autosuggestions.git
 
 
 cat>>~/.zshrc1<<EOF
