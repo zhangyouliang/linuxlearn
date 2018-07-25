720b91de44919abd0355e0120caa379a2415cd3a
YouliangZhang <1577121881@qq.com>
2018-07-25 23:36:31 +0800

    测试

--- tools/zsh/README.md
+++ tools/zsh/README.md
@@ -1,3 +1,4 @@
 
-
+    
+    sh -c "$(curl -fsSL https://gitee.com/whatdy/linuxlearn/raw/master/tools/zsh/install.sh)"
     sh -c "$(curl -fsSL https://gitee.com/whatdy/linuxlearn/raw/master/tools/zsh/zsh.sh)"
--- tools/zsh/zsh.sh
+++ tools/zsh/zsh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sh -c "$(curl -fsSL https://gitee.com/whatdy/linuxlearn/raw/master/tools/zsh/install.sh)"
 cd ~/.oh-my-zsh/plugins/
+
 git clone https://gitee.com/whatdy/zsh-autosuggestions.git
 
 
-cat>>~/.zshrc1<<EOF
+cat>>~/.zshrc<<EOF
 # 防止中文乱码
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
