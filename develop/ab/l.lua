26a4d5baef3a5915aa97097d88caec0aa62acd3d
zhangyoulinag <1577121881@qq.com>
2018-09-30 16:11:20 +0800

    lua 脚本修改

--- develop/ab/l.lua
+++ develop/ab/l.lua
@@ -1,3 +1,10 @@
 wrk.method = "GET"
 wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
 wrk.body = ""
+-- 参考: https://type.so/linux/lua-script-in-wrk.html
+-- lua还提供了几个函数   
+-- setup 函数 、init 函数 、delay函数、request函数 、response函数、done函数。
+function response(status,headers,body)
+    t = os.date("%Y-%m-%d %H:%M:%S", os.time())
+    print("body:",body,t)
+end
